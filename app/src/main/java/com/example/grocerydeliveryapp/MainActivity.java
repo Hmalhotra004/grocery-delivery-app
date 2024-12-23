95cd27fb3fda1fcfb51e9b7d4b9306b0e0b32386
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-12-23 20:57:08 +0530

    navbar

--- app/src/main/java/com/example/grocerydeliveryapp/MainActivity.java
+++ app/src/main/java/com/example/grocerydeliveryapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.grocerydeliveryapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -10,21 +11,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+import com.example.grocerydeliveryapp.R;
 
+
+import com.example.grocerydeliveryapp.databinding.ActivityMainBinding;
 import com.google.firebase.auth.FirebaseAuth;
 
 public class MainActivity extends AppCompatActivity {
 
   public FirebaseAuth firebaseAuth;
-  public Button logoutBtn;
+
+  ActivityMainBinding binding;
+//  public Button logoutBtn;
+
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    binding = ActivityMainBinding.inflate(getLayoutInflater());
     EdgeToEdge.enable(this);
-    setContentView(R.layout.activity_main);
+    setContentView(binding.getRoot());
+    replaceFragment(new HomeFragment());
 
-    logoutBtn = findViewById(R.id.logout_btn);
+//    logoutBtn = findViewById(R.id.logout_btn);
+
+    binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+      int id = item.getItemId();
+
+//      switch (item.getItemId()){
+//        case R.id.home:
+//          replaceFragment(new HomeFragment());
+//          break;
+//
+//        case R.id.user:
+//          replaceFragment(new ProfileFragment());
+//          break;
+//      }
+
+      if (id == R.id.home) {
+        replaceFragment(new HomeFragment());
+      } else if (id == R.id.user) {
+        replaceFragment(new ProfileFragment());
+      } else {
+        return false;
+      }
+      return true;
+    });
 
     firebaseAuth = FirebaseAuth.getInstance();
 
@@ -33,7 +68,7 @@ public class MainActivity extends AppCompatActivity {
       finish();
     }
 
-
+    
     // Handle window insets for edge-to-edge layout
     ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
       Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
@@ -41,13 +76,20 @@ public class MainActivity extends AppCompatActivity {
       return insets;
     });
 
-    logoutBtn.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View view) {
-        firebaseAuth.signOut();
-        startActivity(new Intent(MainActivity.this, LoginActivity.class));
-        finish();
-      }
-    });
+//    logoutBtn.setOnClickListener(new View.OnClickListener() {
+//      @Override
+//      public void onClick(View view) {
+//        firebaseAuth.signOut();
+//        startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//        finish();
+//      }
+//    });
+  }
+
+  private void replaceFragment(Fragment fragment){
+    FragmentManager fragmentManager = getSupportFragmentManager();
+    FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+    fragmentTransaction.replace(R.id.frame_layout,fragment);
+    fragmentTransaction.commit();
   }
 }
