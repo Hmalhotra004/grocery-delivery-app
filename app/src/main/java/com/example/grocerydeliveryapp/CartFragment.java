1fcc7ee898498fae10cdf3a1a33190531e789b93
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-12-29 13:21:33 +0530

    Update CartFragment.java

--- app/src/main/java/com/example/grocerydeliveryapp/CartFragment.java
+++ app/src/main/java/com/example/grocerydeliveryapp/CartFragment.java
@@ -1,42 +1,36 @@
 package com.example.grocerydeliveryapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link CartFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class CartFragment extends Fragment {
+import com.example.grocerydeliveryapp.adapters.CartAdapters;
+import com.example.grocerydeliveryapp.models.CartModel;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
 
-  // TODO: Rename parameter arguments, choose names that match
-  // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
   private static final String ARG_PARAM1 = "param1";
   private static final String ARG_PARAM2 = "param2";
+import java.util.ArrayList;
+import java.util.List;
 
-  // TODO: Rename and change types of parameters
   private String mParam1;
   private String mParam2;
+public class CartFragment extends Fragment {
 
   public CartFragment() {
     // Required empty public constructor
   }
 
-  /**
-   * Use this factory method to create a new instance of
-   * this fragment using the provided parameters.
-   *
-   * @param param1 Parameter 1.
-   * @param param2 Parameter 2.
-   * @return A new instance of fragment CartFragment.
-   */
-  // TODO: Rename and change types and number of parameters
   public static CartFragment newInstance(String param1, String param2) {
     CartFragment fragment = new CartFragment();
     Bundle args = new Bundle();
@@ -45,6 +39,8 @@ public class CartFragment extends Fragment {
     fragment.setArguments(args);
     return fragment;
   }
+  List<CartModel> cartModelList;
+  CartAdapters cartAdapters;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -54,11 +50,32 @@ public class CartFragment extends Fragment {
       mParam2 = getArguments().getString(ARG_PARAM2);
     }
   }
+  RecyclerView cartRecyclerView;
+  TextView emptyCartMessage;
+
+  FirebaseFirestore db;
 
   @Override
   public View onCreateView(LayoutInflater inflater, ViewGroup container,
                            Bundle savedInstanceState) {
     // Inflate the layout for this fragment
     return inflater.inflate(R.layout.fragment_cart, container, false);
+    View view = inflater.inflate(R.layout.fragment_cart, container, false);
+
+    db = FirebaseFirestore.getInstance();
+
+    cartRecyclerView = view.findViewById(R.id.cartItemRec);
+//    emptyCartMessage = view.findViewById(R.id.emptyCartMessage);
+
+    cartRecyclerView.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false));
+
+    cartModelList = new ArrayList<>();
+    cartAdapters = new CartAdapters(getActivity(), cartModelList);
+
+    cartRecyclerView.setAdapter(cartAdapters);
+
+
+    return view;
   }
-}
+  }
+}}
