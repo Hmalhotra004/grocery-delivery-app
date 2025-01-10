a75bab37d609b126e69988e1a3384b7463734579
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2025-01-10 17:47:13 +0530

    Update ProductOrdersModel.java

--- app/src/main/java/com/example/grocerydeliveryapp/models/ProductOrdersModel.java
+++ app/src/main/java/com/example/grocerydeliveryapp/models/ProductOrdersModel.java
@@ -1,15 +1,39 @@
 package com.example.grocerydeliveryapp.models;
 
 public class ProductOrdersModel {
-  public String productId, imageUrl;
-  public int quantity;
+  public String productId, imageUrl,description, name;
+  public int quantity, price;
 
   public ProductOrdersModel() {}
 
-  public ProductOrdersModel(String productId, int quantity,String imageUrl) {
-    this.productId = productId;
-    this.quantity = quantity;
-    this.imageUrl = imageUrl;
+//  public ProductOrdersModel(String productId, int quantity,String imageUrl) {
+//    this.productId = productId;
+//    this.quantity = quantity;
+//    this.imageUrl = imageUrl;
+//  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public Integer getPrice() {
+    return price;
+  }
+
+  public void setPrice(Integer price) {
+    this.price = price;
   }
 
   public String getImageUrl() {
